53c1aedc9364a206a210809e4d926ec36b6c44bb
Agnivesh Manojkumar <agniveshtm2724@gmail.com>
2026-09-14 23:48:47 +0530

    feat(flux): add offline file converter

---
+++ src/flux/__init__.py
@@ -0,0 +1,1 @@
+"""flux - an offline file converter tool"""
---
+++ src/flux/main.py
@@ -0,0 +1,9 @@
+import webview 
+
+
+def main():
+    webview.create_window('flux')
+    webview.start()
+
+if __name__ == "__main__":
+    main()
